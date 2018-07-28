84739fbba0f7e4d999c395dca5af439b54e4c6c4
menaczar <menarondaroad@hotmail.com>
2018-07-28 00:05:42 -0700

    cleaning

--- myServer/app.js
+++ myServer/app.js
@@ -2,19 +2,25 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
 var timestamp = require('time-stamp');
-var port=3000
+var port = 3000
 
 app.post('/log',function(request,response){
   var room=request.query.room;
   var temperature=request.query.temp;
   var humidity = request.query.humidity;
-  var log = "at time: "+timestamp('YYYY-MM-DD HH:mm:ss')+ " room:" + room +", temperature was "+temperature+" and humidity was " +humidity;
+  var log = "at time: " + timestamp('YYYY-MM-DD HH:mm:ss') + 
+  " room:" + room + ", temperature was " + temperature + 
+  " and humidity was " + humidity;
+  
   if(!isNaN(temperature) && !isNaN(humidity)){ //make sure valid reading recieved 
   console.log(log); 
-  var csvLine = timestamp('YYYY-MM-DD HH:mm:ss') + ',' + room + ',' + temperature + ',' + humidity + '\n';
+  var csvLine = timestamp('YYYY-MM-DD HH:mm:ss') + ',' +
+  room + ',' + temperature + ',' + humidity + '\n';
+
   fs.appendFile('log.csv', csvLine, function (err) {
     if (err) throw err;
   })}
+
   response.end("Logged: " + log);
 });
 
